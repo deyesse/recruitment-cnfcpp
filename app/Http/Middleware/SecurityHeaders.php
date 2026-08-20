a0d50e9244ae066bcb5f9f91fe35f02720cfba5f
Esseyed Bouzekri <t23532001@gmail.com>
2026-08-20 21:58:16 +0100

    security resolve 0.4

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -19,34 +19,6 @@ class SecurityHeaders
         /** @var \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response $response */
         $response = $next($request);
 
-        // Content Security Policy (CSP)
-        $csp = "default-src 'self'; " .
-               "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " .
-               "style-src 'self' 'unsafe-inline' https://fonts.bunny.net; " .
-               "font-src 'self' https://fonts.bunny.net data:; " .
-               "img-src 'self' data: blob:; " .
-               "connect-src 'self'; " .
-               "form-action 'self'; " .
-               "base-uri 'self'; " .
-               "frame-ancestors 'self';";
-
-        $response->headers->set('Content-Security-Policy', $csp);
-
-        // Prevent MIME type sniffing
-        $response->headers->set('X-Content-Type-Options', 'nosniff');
-
-        // Prevent Clickjacking
-        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
-
-        // Legacy XSS Protection
-        $response->headers->set('X-XSS-Protection', '1; mode=block');
-
-        // Referrer Policy
-        $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
-
-        // Permissions Policy
-        $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
-
         // Remove information disclosure headers
         $response->headers->remove('X-Powered-By');
         if (function_exists('header_remove')) {
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,8 +22,6 @@
     <link rel="icon" href="/cnfcpp.png" sizes="any">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-    <link rel="preconnect" href="https://fonts.bunny.net" crossorigin="anonymous">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" crossorigin="anonymous" />
 
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
