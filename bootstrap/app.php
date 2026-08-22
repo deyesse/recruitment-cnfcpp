7eeccffc72e318a60e7b10b6f5a531364df5a57c
Esseyed Bouzekri <t23532001@gmail.com>
2026-08-22 10:56:23 +0100

    remove the security headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies("*");
 
         $middleware->web(append: [
-            SecurityHeaders::class,
+                // SecurityHeaders::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
